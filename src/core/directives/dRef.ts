fe64710beb7b38bfece1b644ea53e86182f97c6f
ahibis <nikitonvg@list.ru>
2025-04-18 03:24:39 +0300

    fix: rename :ref to @ref

--- src/core/directives/dRef.ts
+++ src/core/directives/dRef.ts
@@ -1,9 +1,9 @@
 import { createDirective, evalFunc } from "./createDirective";
 
-const dRef = createDirective(":ref", (child, ctx, value) => {
+const dRef = createDirective("@ref", (child, ctx, value) => {
   const func = evalFunc(value);
   const res = func(ctx) as (element: HTMLElement) => void;
   res(child);
-  child.removeAttribute(":ref");
+  child.removeAttribute("@ref");
 });
 export { dRef };
